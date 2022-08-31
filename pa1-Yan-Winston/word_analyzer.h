c0b38289cf6de9b6d4ec295ce811604b6965d5af
Yan_Winston <84288759+Yan-Winston@users.noreply.github.com>
2022-08-31 09:19:01 -0700

    Add files via upload

---
+++ pa1-Yan-Winston/word_analyzer.h
@@ -0,0 +1,16 @@
+// By: Winston Yan
+// On: 5/26/2022
+// For: CS 202 Programming Assignment 1
+
+#ifndef word_analyzer
+#define word_analyzer
+
+using namespace std;
+
+int countLowerCase(string);
+int countUpperCase(string);
+int countVowels(string);
+int countLetters(string);
+string toLower(string);
+
+#endif
